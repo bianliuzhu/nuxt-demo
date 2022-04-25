c3634ea657359555d727309213a0d79acc259e45
Gleason <bianliuzhu@gmail.com>
2022-04-25 16:56:32 +0800

    build: 初始化项目

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: Gleason
+ * @Date: 2022-04-25 16:40:57
+ * @LastEditors: Gleason
+ * @LastEditTime: 2022-04-25 16:49:56
+ */
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+}
